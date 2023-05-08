57cc1a7f9a3b49716c7b9537af0d7c36ebbe74ea
Nima Rahbar <nrahbar@bettercollective.com>
2023-05-08 13:39:58 +0300

    Fixing the icon

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'f37cc8bca7c683541f81');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'ce65a05f2d019a018106');
