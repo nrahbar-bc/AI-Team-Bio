cfec393f8d8260e8c1034542ae9389bf70e872f1
dtsiakiris <dtsiakiris@bettercollective.com>
2023-05-09 12:53:09 +0300

    Added the api js file

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b4fa6cc48a22fd817c50');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '1fdf3c9b4f797c5ce4ad');
