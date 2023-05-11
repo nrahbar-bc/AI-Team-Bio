ee17e46bccf943b30644b862068375b116a59592
afisic92 <afisic@bettercollective.com>
2023-05-11 10:14:56 +0200

    Added submenu in settings

---
+++ AiTeamBioOption.php
@@ -0,0 +1,83 @@
+<?php
+class AiTeamBioOption
+{
+    private $ai_team_bio_options;
+    public function __construct()
+    {
+        add_action('admin_menu', array($this, 'aiTeamBioAddPluginPage'));
+        add_action('admin_init', array($this, 'aiTeamBioPageInit'));
+    }
+    public function aiTeamBioAddPluginPage()
+    {
+        add_options_page(
+            'AI Team Bio', // page_title
+            'AI Team Bio', // menu_title
+            'manage_options', // capability
+            'ai-team-bio', // menu_slug
+            array($this, 'aiTeamBioCreateAdminPage') // function
+        );
+    }
+    public function aiTeamBioCreateAdminPage()
+    {
+        $this->ai_team_bio_options = get_option('ai_team_bio_option_name'); ?>
+        <div class="wrap">
+            <h2>AI Team Bio</h2>
+            <p></p>
+            <?php settings_errors(); ?>
+            <form method="post" action="options.php">
+                <?php
+                settings_fields('ai_team_bio_option_group');
+                do_settings_sections('ai-team-bio-admin');
+                submit_button();
+                ?>
+            </form>
+        </div>
+<?php }
+    public function aiTeamBioPageInit()
+    {
+        register_setting(
+            'ai_team_bio_option_group', // option_group
+            'ai_team_bio_option_name', // option_name
+            array($this, 'aiTeamBioSanitize') // sanitize_callback
+        );
+        add_settings_section(
+            'ai_team_bio_setting_section', // id
+            'Settings', // title
+            array($this, 'aiTeamBioSectionInfo'), // callback
+            'ai-team-bio-admin' // page
+        );
+        add_settings_field(
+            'chatgpt_api_key_0', // id
+            'ChatGPT Api Key', // title
+            array($this, 'chatgptApiKeyCallback'), // callback
+            'ai-team-bio-admin', // page
+            'ai_team_bio_setting_section' // section
+        );
+    }
+    public function aiTeamBioSanitize($input)
+    {
+        $sanitary_values = array();
+        if (isset($input['chatgpt_api_key_0'])) {
+            $sanitary_values['chatgpt_api_key_0'] = sanitize_text_field($input['chatgpt_api_key_0']);
+        }
+        return $sanitary_values;
+    }
+    public function aiTeamBioSectionInfo()
+    {
+    }
+    public function chatgptApiKeyCallback()
+    {
+        printf(
+            '<input class="regular-text" type="text" name="ai_team_bio_option_name[chatgpt_api_key_0]" id="chatgpt_api_key_0" value="%s">',
+            isset($this->ai_team_bio_options['chatgpt_api_key_0']) ? esc_attr($this->ai_team_bio_options['chatgpt_api_key_0']) : ''
+        );
+    }
+}
+if (is_admin()) {
+    $ai_team_bio = new AITeamBioOption();
+}
+/*
+Retrieve this value with:
+$ai_team_bio_options = get_option( 'ai_team_bio_option_name' ); // Array of All Options
+$chatgpt_api_key_0 = $ai_team_bio_options['chatgpt_api_key_0']; // ChatGPT Api Key
+ */
--- ai-team-bio.php
+++ ai-team-bio.php
@@ -12,53 +12,12 @@
  * Text Domain:       ai-team-bio
  */
 
+require dirname(__FILE__) . '/AiTeamBioOption.php';
+
 function bcBlocksAiTeamBioBlockInit()
 {
 	register_block_type_from_metadata(__DIR__ . '/build');
 }
 add_action('init', 'bcBlocksAiTeamBioBlockInit');
-
-function ai_team_bio_settgins_submenu() {
-	add_submenu_page(
-		'options-general.php',
-		'ChatGPT API Key for AI-Team-Bio',
-		'API KEY',
-		'administrator',
-		'api-key-ai-team-bio',
-		'ai_team_bio_settings_page' );
-}
-add_action("admin_menu", "ai_team_bio_settgins_submenu");
-
-function ai_team_bio_settings_page() {
-	$api_key = get_option('chatgpt_api_key'); ?>
-
-	<h1>AI Team Bio</h1>
-	<form method="POST">
-		<label>Enter your API Key</label>
-		<input type="text" name="chatgpt_api_key" value="<?php echo $api_key ? $api_key : '' ?>">
-		<input type="submit" class="button button-primary" value="Save">
-	</form>
- <?php }
-
-function save_api_key() {
-	if ( isset( $_POST['chatgpt_api_key'] ) ) {
-		update_option( 'chatgpt_api_key', $_POST['chatgpt_api_key'] );
-	}
-}
-add_action( 'admin_init', 'save_api_key' );
-
-function ai_team_bio_register_route() {
-	register_rest_route('ai-team-bio/v1', 'api-key', array(
-		'methods'  => 'GET',
-		'callback' => 'ai_team_bio_result'
-	));
-}
-
-function ai_team_bio_result($data) {
-	$api_key = get_option('chatgpt_api_key');
-	return $api_key;
-}
-
-add_action('rest_api_init', 'ai_team_bio_register_route');
 		
 ?>
